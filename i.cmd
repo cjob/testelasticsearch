3a1b0ff9e464e0ba8f8dd87d97bd17a53335f9c9
cjob <christophe.job@oracle.com>
2015-02-10 15:14:27 -0800

    support for no content

--- i.cmd
+++ i.cmd
@@ -1,6 +1,17 @@
 
-sed -e 's/pet/pet%1/g' petstore.json > pet%1store.json
-for %%I IN (pet%1store.json) do @echo pet%1store.json %%~zI >> results
-curl -XPUT "http://localhost:9200/api/json/%1" -d @pet%1store.json
-curl -POST "http://localhost:9200/api/_flush"
-call getsizes.cmd  ..\elasticsearch-1.4.2\data >> results
+sed -e 's/pet/pet%1/g' petstore.json > json\pet%1store.json
+curl -XPUT "http://localhost:9200/addcontent/json/%1" -d @json\pet%1store.json
+curl -XPUT "http://localhost:9200/nocontent/json/" -d @json\pet%1store.json
+curl -POST "http://localhost:9200/addcontent/_flush"
+curl -POST "http://localhost:9200/nocontent/_flush"
+
+
+>output.tmp call getsizes.cmd  .
+>>output.tmp call getsizes.cmd  ..\elasticsearch-1.4.2\data\elasticsearch\nodes\0\indices\
+
+<output.tmp (
+  set /p line1=
+  set /p line2=
+  set /p line3=
+)
+echo %line1% %line2% %line3% >> results
--- r.cmd
+++ r.cmd
@@ -1,5 +1,17 @@
 del results
-del pet*.json
+del json\pet*.json
+
+rem delete indexes
+rem curl -XDELETE 'http://localhost:9200/nocontent
+rem curl -XPOST 'http://localhost:9200/nocontent' -d 
+rem '{
+rem    "mappings" : {
+rem        "json": {
+rem            "_source": {"enabled": false} }
+rem        }
+rem    }
+rem}''
+
 copy petstore.bak petstore.json
 FOR /L %%N IN (1,1,1000) do (
 	call i.cmd  %%N
